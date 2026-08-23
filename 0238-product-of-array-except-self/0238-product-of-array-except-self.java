03dc49e13e6477854a2a2dd5c7cd472fd7c60704
Anshuman Singh <anshuman103019@gmail.com>
2026-08-23 10:59:13 +0530

    Time: 3 ms (24.08%), Space: 65 MB (76.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,8 +5,8 @@ class Solution {
         left[0] = 1;
         for(int i = 1; i<n; i++){
             left[i] = left[i-1]*arr[i-1];
-        }
-        int[] right = new int [n];
+        } 
+        int[] right = new int[n];
         right[n-1] = 1;
         for(int i = n-2; i>=0; i--){
             right[i] = right[i+1]*arr[i+1];
@@ -16,7 +16,5 @@ class Solution {
             ans[i] = left[i]*right[i];
         }
         return ans;
-
-    
     }
 }
