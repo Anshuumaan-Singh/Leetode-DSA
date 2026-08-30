196b271fd25614a7ad9db7a3091a2a517102699c
Anshuman Singh <anshuman103019@gmail.com>
2026-08-30 15:18:38 +0530

    Time: 2 ms (100%), Space: 48.1 MB (83.92%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
-        int m = 0, c = arr[0].length-1;
-        while(m<arr.length && c>=0){
-            if(arr[m][c] == target){
+        int r = 0, c = arr[0].length-1;
+        while(r<arr.length && c>=0){
+            if(arr[r][c] == target){
                 return true;
             }
-            else if(arr[m][c]>target){
+            else if(arr[r][c]>target){
                 c--;
             }
             else{
-                m++;
+                r++;
             }
         }
         return false;
