febfbb10db1d4cd8eac919e11af0e37d1baec8e0
Anshuman Singh <anshuman103019@gmail.com>
2026-09-05 17:56:15 +0530

    Time: 1 ms (60.37%), Space: 42.5 MB (52.01%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,8 +3,8 @@ class Solution {
         int ans = 0;
         for(int i = 0; i<32; i++){
             ans = ans<<1;
-            ans = ans | (n&1);
-            n >>>= 1;
+            ans = ans | (n&1);     //(n&1) == will give the last bit
+            n = n >>> 1;
         }
         return ans;
     }
